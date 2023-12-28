a45f09bc5f21187ebf979395f2e6162f578c152b
Antoskuu <antoning.antonin@gmail.com>
2023-12-28 19:12:08 +0100

    grille test et fonction affichage

---
+++ code/code.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func afficheSudoku(grille [9][9]int) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			fmt.Printf("%2d ", grille[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	grilleSudoku := [9][9]int{
+		{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		{0, 9, 8, 0, 0, 0, 0, 6, 0},
+		{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		{7, 0, 0, 0, 2, 0, 0, 0, 6},
+		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	}
+
+	afficheSudoku(grilleSudoku)
+
+}
