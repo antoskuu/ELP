ebf410643bd28a1f311d6a6eebf48c57f9287aee
Antoine Bois--Berlioz <antoine.boisberlioz@gmail.com>
2024-01-31 18:15:04 +0100

    Update

--- GO/CODE/sudoku_worker.go
+++ GO/CODE/sudoku_worker.go
@@ -6,7 +6,6 @@ import (
 	"time"
 )
 
-// constante à changer selon la taille de la grille
 const TAILLE = 9
 const TAILLE_BLOCK = 3
 
@@ -17,7 +16,7 @@ type problem struct {
 	colonne int
 }
 
-// fonction qui verifie si un chiffre est absent sur une ligne
+// Fonctions de signature pour la verification des possibilités
 func absentSurLigne(k int, grille [TAILLE][TAILLE]int, ligne int) bool {
 	for colonne := 0; colonne < TAILLE; colonne++ {
 		if grille[ligne][colonne] == k {
@@ -27,7 +26,6 @@ func absentSurLigne(k int, grille [TAILLE][TAILLE]int, ligne int) bool {
 	return true
 }
 
-// fonction qui vérifie si un nombre est absent sur une colonnne
 func absentSurColonne(k int, grille [TAILLE][TAILLE]int, colonne int) bool {
 	for ligne := 0; ligne < TAILLE; ligne++ {
 		if grille[ligne][colonne] == k {
@@ -37,7 +35,6 @@ func absentSurColonne(k int, grille [TAILLE][TAILLE]int, colonne int) bool {
 	return true
 }
 
-// fonction qui vérifie si un nombre est absent dans un block
 func absentSurBlock(k int, grille [TAILLE][TAILLE]int, ligne int, colonne int) bool {
 	ligne2 := ligne - ligne%TAILLE_BLOCK
 	colonne2 := colonne - colonne%TAILLE_BLOCK
@@ -56,31 +53,19 @@ func printProb(prob problem) {
 	for _, ligne := range prob.Grille {
 		fmt.Println(ligne)
 	}
-	fmt.Printf("Colonne : %d\nLigne : %d\n", prob.colonne, prob.ligne)
+	//fmt.Printf("Colonne : %d\nLigne : %d\n", prob.colonne, prob.ligne)
 }
-func printProbChan(probChan chan problem) {
-	fmt.Printf("Début du printProbChan\n")
-	for prob := range probChan {
-		for _, ligne := range prob.Grille {
-			fmt.Println(ligne)
-		}
-		fmt.Printf("Colonne : %d\nLigne : %d\n", prob.colonne, prob.ligne)
-	}
-	fmt.Printf("Fin du printProbChan\n")
 
-}
-func printCase(p problem, id int) {
-	fmt.Printf("id: %d, Grille[%d] = %d\n", id, p.ligne, p.Grille[p.ligne])
-}
 func solve(prob problem, problemChan chan problem, quit chan struct{}) {
-
 	if prob.ligne == TAILLE {
+		// Found solution
+		// Print and closing channel
+		fmt.Printf("Solution found !\n")
 		printProb(prob)
 		close(quit)
 		return
 
 	} else if prob.colonne == TAILLE {
-
 		go func() {
 			problemChan <- problem{
 				Grille:  prob.Grille,
@@ -102,17 +87,15 @@ func solve(prob problem, problemChan chan problem, quit chan struct{}) {
 
 	} else {
 		for k := 1; k <= TAILLE; k++ {
-
 			if absentSurBlock(k, prob.Grille, prob.ligne, prob.colonne) && absentSurColonne(k, prob.Grille, prob.colonne) && absentSurLigne(k, prob.Grille, prob.ligne) {
-
-				go func() {
+				go func(k int) {
 					prob.Grille[prob.ligne][prob.colonne] = k
 					problemChan <- problem{
 						Grille:  prob.Grille,
 						ligne:   prob.ligne,
 						colonne: (prob.colonne + 1),
 					}
-				}()
+				}(k)
 			}
 		}
 		return
@@ -124,7 +107,7 @@ func worker(id int, ch chan problem, quit chan struct{}, wg *sync.WaitGroup) {
 	for {
 		select {
 		case x := <-ch:
-			// Received value from the channel
+			// Received problem from ch and solve
 			solve(x, ch, quit)
 
 		case <-quit:
@@ -144,15 +127,15 @@ func main() {
 	numWorkers := 8
 
 	grille := [TAILLE][TAILLE]int{
-		{0, 0, 8, 0, 0, 4, 0, 9, 0},
-		{0, 7, 0, 1, 0, 0, 5, 0, 0},
-		{5, 0, 0, 0, 6, 0, 0, 0, 3},
-		{1, 0, 0, 0, 4, 0, 0, 0, 8},
-		{0, 8, 0, 0, 0, 0, 7, 0, 0},
-		{0, 0, 2, 0, 0, 0, 0, 1, 0},
-		{0, 6, 0, 2, 0, 0, 0, 0, 0},
-		{3, 0, 0, 0, 8, 0, 0, 0, 5},
-		{0, 0, 7, 0, 0, 9, 0, 4, 0},
+		{0, 0, 0, 0, 7, 0, 1, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 0, 4},
+		{0, 0, 0, 0, 0, 6, 0, 7, 0},
+		{0, 7, 0, 0, 9, 0, 8, 0, 0},
+		{4, 0, 0, 0, 0, 0, 0, 5, 0},
+		{0, 0, 2, 3, 0, 0, 0, 9, 6},
+		{0, 0, 3, 2, 0, 0, 0, 0, 7},
+		{0, 5, 0, 0, 8, 0, 9, 0, 0},
+		{1, 0, 0, 0, 0, 9, 0, 6, 0},
 	}
 	prob_base := problem{
 		Grille:  grille,
@@ -162,17 +145,19 @@ func main() {
 	go func() {
 		ch <- prob_base
 	}()
+
 	// Start workers
 	debut := time.Now()
 
 	for i := 1; i <= numWorkers; i++ {
+		fmt.Printf("Worker %d starting\n", i)
 		wg.Add(1)
 		go worker(i, ch, quit, &wg)
-		time.Sleep(time.Second)
 	}
 
 	// Wait for all workers to finish
 	wg.Wait()
+
 	fin := time.Now()
 	fmt.Printf("\nTemps d'execution : %.6fs \n", fin.Sub(debut).Seconds())
 	fmt.Println("Main function finished")
