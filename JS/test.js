8eb84b5db7267826c08f38baa2222c6e29903084
Antoskuu <antoning.antonin@gmail.com>
2024-01-31 18:56:08 +0100

    update tour a tour

--- JS/test.js
+++ JS/test.js
@@ -53,7 +53,8 @@ function grilleInit(valeurInitiale) {
         const ligne = Array(9).fill(valeurInitiale);
         grille.push(ligne);
     }
-
+    
+    ajoutScore(grille, 0, [0, 0, 9, 16, 25, 36, 49, 64, 81]);
     return grille;
 }
 
@@ -75,23 +76,8 @@ function affichagePlateau(grille) {
     }
 }
 
-function peutFormerMot(lettres, mot) {
-    let copieLettres = [...lettres]; // Créer une copie de la liste de lettres pour ne pas la modifier
-
-    for (let lettre of mot) {
-        let index = copieLettres.indexOf(lettre);
-        if (index === -1) {
-            // Si la lettre n'est pas dans la liste, retourner false
-            return false;
-        } else {
-            // Sinon, supprimer la lettre de la liste
-            copieLettres.splice(index, 1);
-        }
-    }
-
     // Si toutes les lettres du mot sont dans la liste, retourner true
-    return true;
-}
+
 
 
 //ajout d'un score
@@ -116,9 +102,39 @@ function ajoutScore(grille, ligne, suite) {
 }
 //ajout d'un mot à la grille
 
+
+// Exemple d'utilisation pour créer une grille 3x4 avec la valeur initiale ""
+const maGrille = grilleInit("");
+
+
+
+
+
+function peutFormerMot(lettres, mot, grille) {
+    if (mot.length > 2 && mot.length <= grille[0].length) {
+        let copieLettres = [...lettres]; // Créer une copie de la liste de lettres pour ne pas la modifier
+    for (let lettre of mot) {
+        let index = copieLettres.indexOf(lettre);
+        if (index === -1) {
+            // Si la lettre n'est pas dans la liste, retourner false
+            return false;
+        } else {
+            // Sinon, supprimer la lettre de la liste
+            copieLettres.splice(index, 1);
+        }
+    }
+    return true;
+        } else {
+        console.log("La longueur du mot ne satisfait pas les conditions.");
+        return false; // Le mot n'a pas été ajouté
+    }
+    
+}
+    
+
 function ajoutMotAGrille(grille, mot, ligne) {
     // Vérifier si la longueur du mot est supérieure à 3 et inférieure à la taille maximale de la grille
-    if (mot.length > 3 && mot.length <= grille[0].length) {
+    if (mot.length > 2 && mot.length <= grille[0].length) {
         // Vérifier si la ligne spécifiée est vide
         if (grille[ligne].every(cellule => cellule === "" || cellule === undefined)) {
             // Ajouter le mot à la ligne de la grille
@@ -129,53 +145,127 @@ function ajoutMotAGrille(grille, mot, ligne) {
         } else {
             console.log("La ligne n'est pas vide. Choisissez une ligne vide.");
         }
-    } else {
-        console.log("La longueur du mot ne satisfait pas les conditions.");
+    } 
+}
+function jeuEstTermine(numero_tour){
+    return numero_tour > 7; // Le jeu se termine après le tour 6
+} 
+
+// function poserQuestion(numero_tour, numero_plateau) {
+//     if (jeuEstTermine(numero_tour)) {
+//         console.log("Le jeu est terminé.");
+//         rl.close();
+//         return;
+//     }
+
+//     rl.question('Entrez un mot : ', (reponse) => {
+//         if (peutFormerMot(main1, reponse, numero_plateau)) {
+//             console.log(`Vous pouvez former le mot ${reponse} avec les lettres de votre main.`);
+//             reponse=reponse.toString();
+//             ajoutMotAGrille(numero_plateau, reponse, numero_tour);
+//             console.log("lettre au hasard: " + tirerLettreAleatoire(pioche));
+//             affichagePlateau(numero_plateau);
+//             poserQuestion(numero_tour + 1, numero_plateau); // Appeler poserQuestion à nouveau pour le prochain tour
+//         } else {
+//             console.log(`Vous ne pouvez pas former le mot ${reponse} avec les lettres de votre main.`);
+//             poserQuestion(numero_tour, numero_plateau);
+//         }
+//     });
+// }
+
+
+
+
+
+
+
+function tirageMain(main) {
+    for (let i = 0; i < 7; i++) {
+        main.push(tirerLettreAleatoire(pioche));
     }
-
-    return false; // Le mot n'a pas été ajouté
 }
 
-// Exemple d'utilisation pour créer une grille 3x4 avec la valeur initiale ""
-const maGrille = grilleInit("");
+// tirageMain(main1);
+
+// console.log(main1);
+// const readline = require('readline');
+
+
+// affichagePlateau(plateau1);
+
+// poserQuestion(1, plateau1);
+
+
+
+plateau1=grilleInit("")
+plateau2=grilleInit("")
+plateau3=grilleInit("")
 
 
 
 
-plateau1=grilleInit()
-plateau2=grilleInit()
-main1=[]
-for (let i = 0; i < 7; i++) {
-    main1.push(tirerLettreAleatoire(pioche));
-}
-
-
-console.log(main1);
 const readline = require('readline');
-
-ajoutScore(maGrille, 0, [0, 0, 9, 16, 25, 36, 49, 64, 81]);
-affichagePlateau(maGrille);
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function poserQuestion() {
+// let mains = [];
+// let nombreDeJoueurs = 2; // Remplacez par le nombre de joueurs que vous voulez
+
+
+
+
+// // Initialiser les mains pour chaque joueur
+// for (let i = 0; i < nombreDeJoueurs; i++) {
+//     mains[i] = [];
+//     tirageMain(mains[i]);
+// }
+
+
+
+
+
+
+
+
+
+let mains = [];
+let plateaux = [];
+let nombreDeJoueurs = 2; // Remplacez par le nombre de joueurs que vous voulez
+
+// Initialiser les mains et les plateaux pour chaque joueur
+for (let i = 0; i < nombreDeJoueurs; i++) {
+    mains[i] = [];
+    tirageMain(mains[i]);
+    plateaux[i] = grilleInit(""); // Supposons que vous ayez une fonction pour créer un nouveau plateau
+}
+
+function poserQuestion(numero_tour, joueur) {
+    if (jeuEstTermine(Math.floor(numero_tour))) {
+        console.log("Le jeu est terminé.");
+        rl.close();
+        return;
+    }
+
+    console.log(`C'est au tour du joueur ${joueur + 1}.`);
+    console.log(`Voici votre main : ${mains[joueur]}`);
     rl.question('Entrez un mot : ', (reponse) => {
-        if (peutFormerMot(main1, reponse)) {
+        if (peutFormerMot(mains[joueur], reponse, plateaux[joueur])) {
             console.log(`Vous pouvez former le mot ${reponse} avec les lettres de votre main.`);
-            ajoutMotAGrille(maGrille, "BONJOUR" + reponse, 1);
+            reponse=reponse.toString();
+            ajoutMotAGrille(plateaux[joueur], reponse, Math.floor(numero_tour));
             console.log("lettre au hasard: " + tirerLettreAleatoire(pioche));
-            rl.close();
+            affichagePlateau(plateaux[joueur]);
+            let prochainJoueur = (joueur + 1) % nombreDeJoueurs; // Alternez le joueur
+            poserQuestion(numero_tour + 0.5, prochainJoueur); // Appeler poserQuestion à nouveau pour le prochain tour
         } else {
             console.log(`Vous ne pouvez pas former le mot ${reponse} avec les lettres de votre main.`);
-            poserQuestion();
+            poserQuestion(numero_tour, joueur); // Si le mot n'est pas valide, le même joueur essaie à nouveau
         }
     });
 }
 
-poserQuestion();
-
-
-
+// Commencer le jeu avec le tour 1 et le joueur 0
+poserQuestion(1, 0);
