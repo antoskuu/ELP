d3f4d0ba05a4abfdc3b8bc0bbc9cf5319fb6ef33
Antoskuu <antoning.antonin@gmail.com>
2024-02-02 16:53:30 +0100

    modif jarnac working

--- JS/test.js
+++ JS/test.js
@@ -110,27 +110,47 @@ const maGrille = grilleInit("");
 
 
 
-function peutFormerMot(lettres, mot, grille) {
-    if (mot.length > 2 && mot.length <= grille[0].length) {
-        let copieLettres = [...lettres]; // Créer une copie de la liste de lettres pour ne pas la modifier
-    for (let lettre of mot) {
-        let index = copieLettres.indexOf(lettre);
-        if (index === -1) {
-            // Si la lettre n'est pas dans la liste, retourner false
-            return false;
-        } else {
-            // Sinon, supprimer la lettre de la liste
-            copieLettres.splice(index, 1);
+// function peutFormerMot(lettres, mot, grille) {
+//     if (mot.length > 2 && mot.length <= 9) {
+//         console.log("mot.length "+mot.length)
+//         let copieLettres = [...lettres]; // Créer une copie de la liste de lettres pour ne pas la modifier
+//     for (let lettre of mot) {
+//         let index = copieLettres.indexOf(lettre);
+//         if (index === -1) {
+//             // Si la lettre n'est pas dans la liste, retourner false
+//             console.log("premier")
+//             return false;
+//         } else {
+//             // Sinon, supprimer la lettre de la liste
+//             copieLettres.splice(index, 1);
+//         }
+//     }
+//     return true;
+//         } else {
+        
+//         console.log("La longueur du mot ne satisfait pas les conditions.");
+//         return false; // Le mot n'a pas été ajouté
+        
+//     }
+    
+// }
+    
+function peutFormerMot(lettres, mot) {
+    if (mot.length > 2) {
+        for (let lettre of mot) {
+            if (!lettres.includes(lettre)) {
+                // Si une lettre du mot n'est pas dans la liste de lettres, retourner false
+                return false;
+            }
         }
+        // Si toutes les lettres du mot sont dans la liste de lettres, retourner true
+        return true;
+    } else {
+        // Si le mot a 2 caractères ou moins, retourner false
+        return false;
     }
-    return true;
-        } else {
-        console.log("La longueur du mot ne satisfait pas les conditions.");
-        return false; // Le mot n'a pas été ajouté
-    }
-    
 }
-    
+
 
 function ajoutMotAGrille(grille, mot, ligne ,main) {
     // Vérifier si la longueur du mot est supérieure à 3 et inférieure à la taille maximale de la grille
@@ -147,6 +167,72 @@ function ajoutMotAGrille(grille, mot, ligne ,main) {
     } 
 }
 
+// function ajoutMotAGrilleJarnac(grille, mot, ligne, mot_de_base, main_joueur) {
+//     // Vérifiez si mot_de_base est une sous-chaîne de mot
+//     if (mot.includes(mot_de_base)) {
+//         // Obtenez les lettres supplémentaires dans mot
+//         let lettresSupplementaires = mot.replace(new RegExp(`[${mot_de_base}]`, 'g'), '');
+        
+//         // Vérifiez si toutes les lettres supplémentaires sont dans main_joueur
+//         for (let lettre of lettresSupplementaires) {
+//             if (!main_joueur.includes(lettre)) {
+//                 return false; // Si une lettre n'est pas dans main_joueur, ne faites rien et quittez la fonction
+//             }
+//         }
+
+//         // Si toutes les lettres supplémentaires sont dans main_joueur, ajoutez mot à grille
+//         grille[ligne] = mot;
+//         return true
+//     }
+// }
+// function ajoutMotAGrilleJarnac(grille, mot, ligne, mot_de_base, main_joueur) {
+//     // Convertir mot_de_base en une chaîne pour la recherche de sous-chaîne
+//     let mot_de_base_str = mot_de_base.join('');
+
+//     // Vérifiez si mot_de_base est une sous-chaîne de mot
+//     if (mot.includes(mot_de_base_str)) {
+//         // Obtenez les lettres supplémentaires dans mot
+//         let lettresSupplementaires = mot.split('').filter(lettre => !mot_de_base.includes(lettre));
+        
+//         // Vérifiez si toutes les lettres supplémentaires sont dans main_joueur
+//         for (let lettre of lettresSupplementaires) {
+//             if (!main_joueur.includes(lettre)) {
+//                 return false; // Si une lettre n'est pas dans main_joueur, ne faites rien et quittez la fonction
+//             }
+//         }
+
+//         // Si toutes les lettres supplémentaires sont dans main_joueur, ajoutez mot à grille
+//         grille[ligne] = mot;
+//         return true;
+//     }
+
+//     // Si mot ne contient pas mot_de_base, retourner false
+//     return false;
+// }
+function ajoutMotAGrilleJarnac(grille, mot, ligne, mot_de_base, main_joueur) {
+    // Convertir mot_de_base en une chaîne pour la recherche de sous-chaîne
+    let mot_de_base_str = mot_de_base.join('');
+
+    // Vérifiez si mot_de_base est une sous-chaîne de mot
+    if (mot.includes(mot_de_base_str)) {
+        // Obtenez les lettres supplémentaires dans mot
+        let lettresSupplementaires = mot.split('').filter(lettre => !mot_de_base.includes(lettre));
+        
+        // Vérifiez si toutes les lettres supplémentaires sont dans main_joueur
+        for (let lettre of lettresSupplementaires) {
+            if (!main_joueur.includes(lettre)) {
+                return false; // Si une lettre n'est pas dans main_joueur, ne faites rien et quittez la fonction
+            }
+        }
+
+        // Si toutes les lettres supplémentaires sont dans main_joueur, ajoutez mot à grille
+        grille[ligne] = mot;
+        return true;
+    }
+
+    // Si mot ne contient pas mot_de_base, retourner false
+    return false;
+}
 
 function retirerLettreDeMain(main, lettre) {
     const index = main.indexOf(lettre);
@@ -314,7 +400,18 @@ function jarnacSupprimerLigne(grille, reponse) {
     grille.push(Array(9).fill(""));
 }
 
+function jarnacSupprimerLettres(main_joueur, mot) {
+    // Convertir le mot en tableau de lettres
+    let lettres = mot.split('');
 
+    // Supprimer les lettres du mot de la main du joueur
+    for (let lettre of lettres) {
+        let index = main_joueur.indexOf(lettre);
+        if (index !== -1) {
+            main_joueur.splice(index, 1);
+        }
+    }
+}
 
 
 let mains = [];
@@ -416,30 +513,50 @@ function poserQuestion(numero_ligne, joueur) {
             if (reponse=="R") {
                 poserQuestion(numero_ligne, joueur)
             }
-            else if (parseInt(reponse)<= 0 || parseInt(reponse)>numero_ligne[(joueur + 1) % nombreDeJoueurs]) {
+            else if (parseInt(reponse)<= 0 || parseInt(reponse)>=numero_ligne[(joueur + 1) % nombreDeJoueurs]) {
                 console.log('Vous devez entrer une réponse valide');
                 poserQuestion(numero_ligne, joueur)
             }
 
-            else if (parseInt(reponse)> 0 && parseInt(reponse)<=numero_ligne[(joueur + 1) % nombreDeJoueurs]){
+            else if (parseInt(reponse)> 0 && parseInt(reponse)<numero_ligne[(joueur + 1) % nombreDeJoueurs]){
             main_temporaire=[]
+            mot_de_base=[]
+            main_totale=[]
             longueur=0
             for (let element of plateaux[(joueur + 1) % nombreDeJoueurs][reponse]) {
                 if (element !== "") {
                     longueur++;
-                    main_temporaire.push(element);
+                    mot_de_base.push(element);
                 }
             }
             for (let element of mains[(joueur + 1) % nombreDeJoueurs]) {
                 main_temporaire.push(element);
             }
+            for (let element of main_temporaire) {
+                main_totale.push(element);
+            }
+            for (let element of mot_de_base) {
+                main_totale.push(element);
+            }
             console.log(`Vous devez former un mot de plus de ${longueur} lettres avec:`);
-            console.log(main_temporaire);
+            console.log(mot_de_base + " obligatoirement");
+            console.log("et au moins une lettre de " +main_temporaire);
             rl.question('Quel mot formez vous?', (reponse2) => {
-              if (reponse2.length>longueur && peutFormerMot(main_temporaire, reponse2, plateaux[joueur]))
-               { ajoutMotAGrille(plateaux[joueur], reponse2, numero_ligne[joueur], main_temporaire);
+                console.log("reponse2.length "+reponse2.length)
+                console.log("longueur " +longueur)
+                console.log(main_totale)
+                console.log("peutformermot "+peutFormerMot(main_totale, reponse2))
+
+
+              if (reponse2.length>longueur && peutFormerMot(main_totale, reponse2))
+               { 
+                console.log("Le nouveau mot est plus long que l'ancien");
+                console.log(ajoutMotAGrilleJarnac(plateaux[joueur], reponse2, numero_ligne[joueur], mot_de_base, main_temporaire))
+                if (ajoutMotAGrilleJarnac(plateaux[joueur], reponse2, numero_ligne[joueur], mot_de_base, main_temporaire)){
 
                 jarnacSupprimerLigne(plateaux[(joueur + 1) % nombreDeJoueurs], reponse);
+
+                jarnacSupprimerLettres(mains[(joueur + 1) % nombreDeJoueurs], reponse2, reponse2);
 
                 console.log(`Vous avez volé la ligne ${reponse} et formé le mot ${reponse2}`);
                 console.log(`Voici votre nouveau plateau:`);
@@ -449,6 +566,7 @@ function poserQuestion(numero_ligne, joueur) {
                 numero_ligne[joueur] = numero_ligne[joueur] + 1;
                 numero_ligne[(joueur + 1) % nombreDeJoueurs] = numero_ligne[(joueur + 1) % nombreDeJoueurs] - 1;
                 poserQuestion(numero_ligne, joueur)
+               }
               }
               else { 
                 console.log("Le nouveau mot n'est pas plus long que l'ancien");
